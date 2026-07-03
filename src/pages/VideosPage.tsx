3396af0e68969d132555273656fb97eed14e6c4f
Sureka <surekaneonlabz@gmail.com>
2026-07-03 16:15:44 +0530

    mobile response

--- src/pages/VideosPage.tsx
+++ src/pages/VideosPage.tsx
@@ -1,1 +1,12 @@
-export { default } from '@/app/dashboard/videos/page';
+import { useEffect } from 'react';
+import { useRouter } from 'next/router';
+
+export default function VideosPage() {
+  const router = useRouter();
+
+  useEffect(() => {
+    router.replace('/dashboard/videos');
+  }, [router]);
+
+  return null;
+}
